c19dffdd49142e3dd1c8fc3cf69f79d2da2bdcc5
Nilesh Londhe <lvnilesh@users.noreply.github.com>
2015-10-16 14:45:58 -0700

    Update solo.rb

--- solo.rb
+++ solo.rb
@@ -1,2 +1,2 @@
 cookbook_path File.join(File.dirname(File.expand_path(__FILE__)), "cookbooks")
-json_attribs File.join(File.dirname(File.expand_path(__FILE__)), "node.json")
+json_attribs File.join(File.dirname(File.expand_path(__FILE__)), "dna.json")
